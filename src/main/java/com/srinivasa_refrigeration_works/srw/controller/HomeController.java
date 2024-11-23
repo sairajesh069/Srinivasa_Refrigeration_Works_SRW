286fb108337afbf4b2516107c2777ae9456bfe05
sai rajesh <sairajesh.srk@gmail.com>
2024-11-23 15:52:18 +0530

    add index.html for redirect to home page on reload and add comments to existing code

--- src/main/java/com/srinivasa_refrigeration_works/srw/controller/HomeController.java
+++ src/main/java/com/srinivasa_refrigeration_works/srw/controller/HomeController.java
@@ -3,13 +3,19 @@ package com.srinivasa_refrigeration_works.srw.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";
+    // Redirects to the home page
+    @GetMapping("/")
+    public String redirectToHome() {
+        return "redirect:/SRW/home"; // Redirects to "/SRW/home"
     }
+
+    // Displays the home view
+    @GetMapping("/SRW/home")
+    public String home() {
+        return "home"; // Returns the "home" view
+    }    
     
 }
