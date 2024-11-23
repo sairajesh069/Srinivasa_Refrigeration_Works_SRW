a48c976b2698eb9da20d0ee4768aaf6766b9a601
sai rajesh <sairajesh.srk@gmail.com>
2024-11-23 11:56:54 +0530

    create Spring Boot app with home page and tested functionality

---
+++ src/main/java/com/srinivasa_refrigeration_works/srw/SrwApplication.java
@@ -0,0 +1,13 @@
+package com.srinivasa_refrigeration_works.srw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srinivasa_refrigeration_works/srw/SrwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srinivasa_refrigeration_works.srw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
